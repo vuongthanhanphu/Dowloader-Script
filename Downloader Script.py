2488e4947d4281fb57e069853e52ca46ec921a55
VuongAnPhu <125387093+vuongthanhanphu@users.noreply.github.com>
2024-04-27 17:17:02 +0700

    Add files via upload

--- Downloader Script.py
+++ Downloader Script.py
@@ -4,10 +4,20 @@ import m3u8
 import subprocess
 
 # In[2]:
-url = "https://hls.streamcdn.xyz/m3u8/57a0c117-cd8d-540d-9ac7-c8fb006fc145"
+url = "https://vip.opstream17.com/20240320/2896_004a9094/3000k/hls/mixed.m3u8"
 r = requests.get(url)
+#print(r.text)
 
-m3u8 = m3u8.loads(r.text)
+playlist = m3u8.loads(r.text)
+#playlist.data['segments'][0]['uri']
 
-#r = requests.get(m3u8.data['segments'][0]['uri'])
+r = requests.get(playlist.data['segments'][0]['uri'])
+
+with open("video.ts", 'wb') as f:
+    for segment in playlist.data['segments']:
+        url = segment['uri']
+        r = requests.get(url)
+        f.write(r.content)
+
+#subprocess.run(['ffmpeg', '-i', 'vidl.ts', 'vidl.mp4'])
 # %%
