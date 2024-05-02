52cceccf265489572e5100187c7d2e3f0d8c6f3b
VuongAnPhu <125387093+vuongthanhanphu@users.noreply.github.com>
2024-05-02 10:22:39 +0700

    Add files via upload

--- Downloader Script.py
+++ Downloader Script.py
@@ -4,20 +4,24 @@ import m3u8
 import subprocess
 
 # In[2]:
-url = "https://vip.opstream17.com/20240320/2896_004a9094/3000k/hls/mixed.m3u8"
+url = "https://hd.hdbophim.com/20220225/158_4072dce1/1000k/hls/mixed.m3u8" #url of video with m3u8 format
 r = requests.get(url)
 #print(r.text)
 
 playlist = m3u8.loads(r.text)
 #playlist.data['segments'][0]['uri']
 
-r = requests.get(playlist.data['segments'][0]['uri'])
+#r = requests.get("https://hd.hdbophim.com/20220225/158_4072dce1/1000k/hls/" + playlist.data['segments'][0]['uri'])
 
-with open("video.ts", 'wb') as f:
+with open("Song Song.ts", 'wb') as f:
     for segment in playlist.data['segments']:
-        url = segment['uri']
-        r = requests.get(url)
-        f.write(r.content)
+        try:
+            url = "https://hd.hdbophim.com/20220225/158_4072dce1/1000k/hls/" + segment['uri']
+            r = requests.get(url)
+            f.write(r.content)
+        except Exception as e:
+            print("Wrong" + url)
+            continue
 
 #subprocess.run(['ffmpeg', '-i', 'vidl.ts', 'vidl.mp4'])
 # %%
