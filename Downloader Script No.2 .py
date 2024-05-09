73948a39a6f97fabdd34e78113909b893f5c48d2
VuongAnPhu <125387093+vuongthanhanphu@users.noreply.github.com>
2024-05-09 15:52:50 +0700

    Add files via upload

--- Downloader Script No.2 .py
+++ Downloader Script No.2 .py
@@ -3,23 +3,43 @@ import requests
 import m3u8
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
+import sys
+import time
 
 # In[2]:
 # Download movies from source
 def download(url, name):
-    print("Has come")
+    # Start Timer
+    start_time = time.time()
+    
+    # Get url
     r = requests.get(url)
-    print('Loaded url')
+    print(f'Loaded url {name}')
     playlist = m3u8.loads(r.text)
+
+    # Download
+    j = 0
     with open(f"{name}.ts", 'wb') as f:
         for segment in playlist.data['segments']:
             try:
                 uri = url.replace('mixed.m3u8', '') + segment['uri']
                 r = requests.get(uri)
                 f.write(r.content)
+
+                # Downloaded status
+                message = f"{name} downloaded: " + str(j) + "/" + str(len(playlist.data['segments']))
+                sys.stdout.write("\r" + message)
+                sys.stdout.flush()
+                j += 1
+                
             except Exception as e:
-                print("Wrong " + uri)
+                #print("Wrong " + uri)
                 continue
+    
+    # End Timer
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f" Done {name}: {elapsed_time:.2f} seconds")
 
 # main
 def main():
@@ -35,8 +55,7 @@ def main():
         for url in urls:
             exe.submit(download, url, names[i])
             i += 1
-    print('Done')
- 
+
 # Entry point
 if __name__ == '__main__':
     main()
